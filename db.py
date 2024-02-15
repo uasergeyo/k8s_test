b03f08a0edeba0213e103d12a64c2ba0b79a232b
Sergey <balashevych.sergey@gmail.com>
2024-02-15 15:21:57 +0200

    add: .env file

--- db.py
+++ db.py
@@ -1,7 +1,14 @@
 from mongoengine import connect,Document, ListField, StringField, URLField, FloatField, DateField
 from mongoengine import Document, ListField, StringField, URLField
+import os
 
-connect(db="test_k8s", host="localhost", port=27017)
+# MongoDB connection details
+MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
+MONGO_PORT = int(os.environ.get('MONGO_PORT', 27017))
+MONGO_DB = os.environ.get('MONGO_DB', 'test_k8s')
+
+# Connect to MongoDB
+connect(db=MONGO_DB, host=MONGO_HOST, port=MONGO_PORT)
 
 
 class Numbers(Document):
